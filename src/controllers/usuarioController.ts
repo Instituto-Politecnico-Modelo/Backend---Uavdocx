cd7c967bea728bdaabc31317936d6aa22e694f37
valenh5 <alumno25.huerga.valentin@ipm.edu.ar>
2025-09-20 12:01:00 -0300

    Reclamos y usuarios

--- src/controllers/usuarioController.ts
+++ src/controllers/usuarioController.ts
@@ -9,6 +9,16 @@ import validator from 'validator';
 import { enviarCorreoVerificacion, enviarCorreoReset } from '../utils/email'; 
 import { sequelize } from '../config/db'; 
 
+export const obtenerTodosUsuarios = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const usuarios = await Usuario.findAll();
+    res.status(200).json(usuarios);
+  } catch (error) {
+    console.error('Error al obtener usuarios:', error);
+    res.status(500).json({ mensaje: 'Error al obtener usuarios', error });
+  }
+};
+
 const SECRET_KEY: string = process.env.CLAVE || '';
 
 export const registrarUsuario = async (req: Request, res: Response): Promise<void> => {
